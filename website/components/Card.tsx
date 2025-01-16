32a73c9796b1182783d33068edfa5f1d77bb12b9
Christian Martinez <hello@christianbmartinez.com>
2025-01-15 23:04:46 -0700

    Example had a bug

--- website/components/Card.tsx
+++ website/components/Card.tsx
@@ -13,7 +13,6 @@ export const Card: React.FC<CardProps> = ({
     return (
         <div className='size-full relative flex items-center justify-center border border-neutral-900 bg-black px-8 py-32'>
             <div className='absolute left-4 top-4 text-sm text-neutral-50'>
-
                 {name}
             </div>
             <CodeViewButton slug={slug} />
