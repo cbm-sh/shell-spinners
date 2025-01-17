34d740de6bdcfc4e9140855d8f0bd6cbd2760260
Christian Martinez <hello@christianbmartinez.com>
2025-01-17 09:20:32 -0700

    Add TopBar component to announce v2, change colors back to white

--- website/components/CliLoader.tsx
+++ website/components/CliLoader.tsx
@@ -10,11 +10,7 @@ type LoaderProps = {
 
 export const CliLoader: React.FC<LoaderProps> = ({ speed, keyframes, className }) => {
   const [currentFrame, setCurrentFrame] = useState(keyframes[0]);
-  const [colorIndex, setColorIndex] = useState(0);
-  const colors = [
-    'white', 'yellow', 'cyan', 'lime', 'magenta', 'red', 'aqua', 'fuchsia', 'lightyellow', 'lightcyan', 'lightgreen',
-    'lightpink', 'gold', 'orange', 'greenyellow', 'springgreen', 'deepskyblue', 'dodgerblue', 'chartreuse', 'lawngreen'
-  ];
+
   useEffect(() => {
     let index = 0;
     const interval = setInterval(() => {
@@ -25,16 +21,8 @@ export const CliLoader: React.FC<LoaderProps> = ({ speed, keyframes, className }
     return () => clearInterval(interval);
   }, [keyframes, speed]);
 
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setColorIndex((prev) => (prev + 1) % colors.length);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [colors.length]);
-
   return (
-  <div style={{color: colors[colorIndex] }} className={className ?? `relative text-4xl font-mono flex flex-col justify-center items-center overflow-hidden`}>{currentFrame}</div>
+  <div className={className ?? `relative text-4xl font-mono flex flex-col justify-center items-center overflow-hidden`}>{currentFrame}</div>
   )
 };
 
