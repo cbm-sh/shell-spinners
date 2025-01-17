37bbca37fb395bb34f67682ade74583cb91d67fa
Christian Martinez <hello@christianbmartinez.com>
2025-01-16 19:15:37 -0700

    Padding fix

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -10,10 +10,10 @@ const Home = () => {
           <h1 className='text-balance relative z-50 font-semibold text-neutral-100 text-4xl md:text-5xl text-center'>
           Cli Loaders
           </h1>
-          <p className='relative z-50 text-neutral-200 text-center pt-6'>
+          <p className='relative z-50 text-neutral-200 text-center py-6'>
           A collection of animated CLI loaders for your projects.
           </p>
-          <div className='pt-12 text-center relative z-50'>
+          <div className='pt-2 text-center relative z-50'>
             <pre>
               <code className='w-full border bg-black border-neutral-800 p-4'>npm install cli-loaders
               <CopyButton code='npm install cli-loaders' className='p-2 inline-flex ml-2 relative -right-2 top-1 cursor-pointer transition border border-neutral-800 hover:bg-neutral-900'/></code>
