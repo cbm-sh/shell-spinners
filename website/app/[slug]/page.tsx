63566ba8a1096c1690cad5eed3aae13ac8f63601
Christian Martinez <hello@christianbmartinez.com>
2025-01-26 22:46:34 -0700

    Loaders needed to be called

--- website/app/[slug]/page.tsx
+++ website/app/[slug]/page.tsx
@@ -12,7 +12,6 @@ import { Button } from '@/components/ui/Button';
 import { getLoaders } from '@/lib/helpers/get-loaders';
 import type { LoaderProps } from '@/types';
 import Link from 'next/link';
-import { memo } from 'react';
 import { HiArrowRight } from 'react-icons/hi';
 
 export const generateStaticParams = async () =>
@@ -22,8 +21,8 @@ export const generateStaticParams = async () =>
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 	const { slug } = await params;
-	const loader: LoaderProps = await getLoaders[slug as keyof typeof getLoaders];
-	const { category, keyframes, speed } = loader as LoaderProps;
+	const loader: LoaderProps = getLoaders[slug as keyof typeof getLoaders];
+	const { category, keyframes, speed } = loader;
 	const loaderKeys = Object.keys(getLoaders);
 	const currentIndex = loaderKeys.indexOf(slug);
 	const nextLoader = loaderKeys[(currentIndex + 1) % loaderKeys.length];
@@ -31,7 +30,7 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 	return (
 		<>
 			<section className='px-6 py-12'>
-				<h1 className='relative text-center text-4xl font-semibold text-balance text-neutral-50 md:text-5xl'>
+				<h1 className='relative text-center text-4xl font-semibold text-balance text-neutral-100 md:text-5xl'>
 					{slug.replace(/_/g, ' ')}
 				</h1>
 				<p className='relative py-6 text-center text-neutral-50'>
@@ -39,12 +38,11 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 				</p>
 			</section>
 			<section className='w-full border border-x-0 border-b-0 border-t-neutral-800 p-6'>
-				<div className='relative flex min-h-96 flex-col items-center justify-center overflow-hidden border border-neutral-800 p-6'>
+				<div className='relative flex min-h-96 flex-col items-center justify-center overflow-hidden border border-neutral-800 bg-black p-6'>
 					<div className='absolute top-4 z-40 flex w-full flex-row items-center justify-between px-4'>
 						<BackButton />
 						<Link href={`/${nextLoader}`}>
 							<Button
-								className=''
 								variant='quinary'
 								icon={<HiArrowRight size={16} />}
 								aria-label='Next Loader'
@@ -55,13 +53,13 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 					<Renderer speed={speed} keyframes={keyframes} category={category} />
 				</div>
 				<div className='mt-6 space-y-6'>
-					<h1 className='text-md text-neutral-50'>Examples</h1>
+					<h1 className='text-md font-light text-neutral-50'>Examples</h1>
 					<StandardExample name={slug} speed={speed} />
 					<CustomExample keyframes={keyframes} />
 					<ZeroDependencyExample speed={speed} keyframes={keyframes} />
-					<p className='text-sm text-neutral-50'>Usage in Oh My Zsh</p>
+					<p className='text-sm font-light text-neutral-50'>Usage in Oh My Zsh</p>
 					<OhMyZshExample speed={speed} keyframes={keyframes} />
-					<p className='text-sm text-neutral-50'>Usage in Next.js</p>
+					<p className='text-sm font-light text-neutral-50'>Usage in Next.js</p>
 					<NextJsExample />
 					<NextJsComponentExample speed={speed} keyframes={keyframes} />
 				</div>
@@ -70,4 +68,4 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 	);
 };
 
-export default memo(Page);
+export default Page;
